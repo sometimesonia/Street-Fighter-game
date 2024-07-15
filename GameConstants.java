46c640420724346cf51bca5714776d6f5fdb7826
sonia <sonia_2020bite074@nitsri.ac.in>
2024-07-15 15:00:17 +0530

    Add files via upload

---
+++ GameConstants.java
@@ -0,0 +1,18 @@
+package com.bmpl.streetfighter.utils;
+
+public interface GameConstants {
+	String TITLE = "Street Fighter By Brain Mentors";
+	int SCREENWIDTH = 1800;
+	int SCREENHEIGHT = 1000;
+	int GROUND = SCREENHEIGHT - 100;
+	int SPEED = 10;
+	String HULK_IMG = "hulk.png";
+	String THOR_IMG = "thor_flip.png";
+	String SPLASH_BG = "splash_bg.jpg";
+	int IDLE = 1;
+	int WALK = 2;
+	int PUNCH = 3;
+	int KICK = 4;
+	int SECONDARY_PUNCH = 5;
+	int HIT = 6;
+}
---
+++ MessageReader.java
@@ -0,0 +1,5 @@
+package com.bmpl.streetfighter.utils;
+
+public class MessageReader {
+
+}
